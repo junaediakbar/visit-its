031d1ff1d917986624fe3feabcfb518fe5ffe7a9
Fajar Satria <72863287+Fajar19051@users.noreply.github.com>
2021-12-08 02:33:45 +0700

    add routing login & register

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KunjunganController;
 use App\Http\Controllers\KunjunganUserController;
+use App\Http\Controllers\CustomAuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,19 +15,23 @@ use App\Http\Controllers\KunjunganUserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
+Route::get('/', [CustomAuthController::class, 'dashboard']);
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::get('admin-login', [CustomAuthController::class, 'admin'])->name('admin-login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::post('custom-admin-login', [CustomAuthController::class, 'customAdminLogin'])->name('admin-login.custom');
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
 Route::name('admin.')->group(function () {
     Route::resource('admin/kunjungans', KunjunganController::class);
 });
-
-
 Route::name('user.')->group(function () {
     Route::resource('user/kunjungans', KunjunganUserController::class);
 
 });
-
 Route::get('about', function(){
     return view('about');
 });
