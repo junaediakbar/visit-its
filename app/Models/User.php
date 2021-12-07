089ab6f8c8faf958f30ed0659660798dcea2a88b
Fajar Satria <72863287+Fajar19051@users.noreply.github.com>
2021-12-08 02:35:19 +0700

    tambah fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'nik',
+        'tanggal_lahir',
+        'instansi',
+        'jabatan',
     ];
 
     /**
